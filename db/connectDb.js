f8c9c048652461c9e215abc902ca15e817c61e80
Sriaman245 <128726399+Sriaman245@users.noreply.github.com>
2025-08-14 21:34:30 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 // i have taken this code from topcoder(mongoose connect mongodb)
 const connectDb = async () => {
     try {
-      const conn = await mongoose.connect(`process.env.MONGODB_URI`, {
+      const conn = await mongoose.connect(process.env.MONGODB_URI, {
         // useNewUrlParser: true,
       });
       console.log(`MongoDB Connected: ${conn.connection.host}`);
